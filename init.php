9ae9e24dd2776e2224e32e699bb876116188e8ed
Андрей <andrey.buharov@gmail.com>
2026-06-22 08:39:41 +0300

    добавил пункты меню в файл init.php.

--- init.php
+++ init.php
@@ -1,2 +1,10 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 defined('EVENTCONFIG_VERSION') OR define('EVENTCONFIG_VERSION', '2.0.8');
+
+Kohana::$config->load('adm')
+    ->set('eventtype', array(
+        'title' => 'Типы событий',
+        'url' => 'eventConfig',
+        'icon' => 'fa-calendar',
+        'order' => 110,
+    ));
